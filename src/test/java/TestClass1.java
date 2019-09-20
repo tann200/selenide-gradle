4abe9d1fea5be0f7a4141078ec24f16c35093568
Tanel Veere <tanel.veere@bigbank.ee>
2019-09-20 11:32:00 +0300

    basic selenide example

--- src/test/java/TestClass1.java
+++ src/test/java/TestClass1.java
@@ -1,15 +1,27 @@
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 
 public class TestClass1 {
     private final Logger LOGGER = LoggerFactory.getLogger(TestClass1.class);
 
+    private static SelenideElement googleSearch = $(By.xpath("//*[@id=\"tsf\"]/div[2]/div[1]/div[1]/div/div[2]/input"));
+
 
     @Test
     public void Test1() {
+
         LOGGER.info("This is TestClass1");
+        open("https://www.google.com");
+        googleSearch.setValue("Searching");
+
     }
 
     @Test
